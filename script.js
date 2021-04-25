67461cc23f7fc6282c837c97c1091976dfdcbd7e
moosius1 <moe20012@byui.edu>
2021-04-24 18:30:26 -0600

    updates

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-var d = new Date();
-var n = d.getFullYear();
-document.getElementById("currentYear").innerHTML = n;
+try{
+    let options = {
+        weekday: "long",
+        day: "numberic",
+        month: "long",
+        year: "numeric"
+    };
+    document.getElementById(
+        "currentdate2"
+    ).textContent = new Date().toLocaleDateString("en-US", options);
+}
