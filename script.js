43f2e332b6f9e1f9b1702e696e65ebe5687b2a09
moosius1 <moe20012@byui.edu>
2021-04-24 19:10:15 -0600

    update

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-try{
-    let options = {
-        weekday: "long",
-        day: "numberic",
-        month: "long",
-        year: "numeric"
-    };
-    document.getElementById(
-        "currentdate2"
-    ).textContent = new Date().toLocaleDateString("en-US", options);
-} catch (e) {
-    alert("Browser doesn't support Locale");
-
-}
+document.querySelector("lastmod").textContent = document.lastmod;
