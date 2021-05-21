420903bf23ad30212c8dfbc6896d7083cf3373df
moosius1 <imoe61@gmail.com>
2021-05-20 22:17:21 -0600

    updates

--- lesson5/bom.js
+++ lesson5/bom.js
@@ -2,16 +2,23 @@ const fav = document.querySelector('#fav');
 const button = document.querySelector('button');
 const output = document.querySelector('.list');
 
-button.addEventListener('click', () => {
-    if (fav.value != '' ) {
+function what(){
+    
+    if (fav.value != '') {
         let li = document.createElement('li');
-        let deletebutton = document.createElement
-        ('button');
-        li.textContent = fav.value;
+        let deletebutton = document.createElement ('button');
+        li.textContent= fav.value;
         deletebutton.textContent = '❌';
-
         li.append(deletebutton);
         output.append(li);
+        deletebutton.addEventListener('click', function()
+        {
+            output.removeChild(li);
+            fav.focus;
+        })
     }
+}
 
-});
+
+
+button.addEventListener('click', what)
