7d37611532fd1782c83c6f9476aa7a8100694b35
moosius1 <imoe61@gmail.com>
2021-05-29 11:56:49 -0600

    update

--- lesson4/script.js
+++ lesson4/script.js
@@ -1,3 +1,4 @@
 function toggleMenu() {
     document.getElementsByClassName("links")[0].classList.toggle("responsive");
+    document.getElementsByClassName("menu")[0].classList.replace("menu","Menu ✖")
 }
