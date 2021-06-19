4c7eb4bb2cc6093fd25ef765ccc67fd09a5bc90e
moosius1 <imoe61@gmail.com>
2021-06-19 17:44:25 -0600

    update w6 stuff

--- lesson9/script.js
+++ lesson9/script.js
@@ -13,3 +13,17 @@ console.log(thedate.getDay());
 if (thedate.getDay() ==0,1,2,3,4,5) {
     document.querySelector('#banner').style.display = 'none'
 }
+
+
+const temp = parseInt(document.getElementById("temp").textContent);
+const wSpeed = parseInt(document.getElementById("Speed").textContent);
+
+let wChill = 35.74 + (0.6215*temp) - 35.75*(wSpeed**0.16) + 0.4275*temp*(wSpeed**0.16);
+console.log("testing");
+
+document.getElementById("output").innerHTML=Math.round(wChill);
+
+console.log(wChill);
+
+console.log(temp);
+console.log(wSpeed);
