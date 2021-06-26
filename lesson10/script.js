75eaf7af92ec5d0063271aecd9f5a8008c2670b6
moosius1 <imoe61@gmail.com>
2021-06-26 16:56:22 -0600

    updates

--- lesson10/script.js
+++ lesson10/script.js
@@ -14,14 +14,77 @@ if (thedate.getDay() ==0,1,2,3,4,5) {
     document.querySelector('#banner').style.display = 'none'
 }
 
+fetch("https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=78c3635114fcdf69ed38df35765e5249&units=imperial")
 
+
+.then(function (data){
+    return data.json();
+})
+.then(function(jsonObject){
+    
+    const current = jsonObject['main']['temp'];
+    const currentDescription = jsonObject['weather'][0]['description'];
+    const windSpeed = jsonObject['wind']['speed'];
+    const humid = jsonObject['main']['humidity'];
+    const name = jsonObject['name'];
+    
+
+
+console.log(jsonObject);
+document.getElementById('townName').textContent = name;
+document.getElementById('temp').textContent = current;
+document.getElementById('High').textContent = currentDescription;
+document.getElementById('Speed').textContent = windSpeed;
+document.getElementById('Humidity').textContent = humid+"%";
+
+const imagesrc = 'https://openweathermap.org/img/w/' + jsonObject.weather[0].icon + '.png';
+const desc = jsonObject.weather[0].description;
+
+
+
+fetch("https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=78c3635114fcdf69ed38df35765e5249&units=imperial")
+    .then((response) => response.json())
+    .then((jsObject) =>{
+        console.log(jsObject);
+
+        let day = 0;
+        const dayofWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+        const fiveDayForecast = jsObject.list.filter( forecast => forecast.dt_txt.includes('18:00:00'));
+        console.log(fiveDayForecast);
+
+        fiveDayForecast.forEach(x=>{
+            let d = new Date(x.dt_txt);
+            console.log(d);
+            document.getElementById(`dayofweek${day+1}`).textContent = dayofWeek[d.getDay()];
+            document.getElementById(`forecast${day+1}`).textContent = x.main.temp;
+            document.getElementById(`foreimage1`).src = 'https://openweathermap.org/img/w/' +fiveDayForecast[0].weather[0].icon +'.png';
+            document.getElementById(`foreimage2`).src = 'https://openweathermap.org/img/w/' +fiveDayForecast[1].weather[0].icon +'.png';
+            document.getElementById(`foreimage3`).src = 'https://openweathermap.org/img/w/' +fiveDayForecast[2].weather[0].icon +'.png';
+            document.getElementById(`foreimage4`).src = 'https://openweathermap.org/img/w/' +fiveDayForecast[3].weather[0].icon +'.png';
+            document.getElementById(`foreimage5`).src = 'https://openweathermap.org/img/w/' +fiveDayForecast[4].weather[0].icon +'.png';
+            
+            day++
+
+            
+        });
+    });
+
+})
+
+
+
+.then(function(windChill){
 const temp = parseInt(document.getElementById("temp").textContent);
+
 const wSpeed = parseInt(document.getElementById("Speed").textContent);
 
 let wChill = 35.74 + (0.6215*temp) - 35.75*(wSpeed**0.16) + 0.4275*temp*(wSpeed**0.16);
 
 
 document.getElementById("output").innerHTML=Math.round(wChill);
+
+})
 
 const firstName = document.getElementById('nameOne');
 const secondName = document.getElementById('nameTwo');
