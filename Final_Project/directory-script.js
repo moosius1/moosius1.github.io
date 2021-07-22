e6b3591dd72493c897f6854aace83c4a71ade723
moosius1 <imoe61@gmail.com>
2021-07-22 00:12:11 -0600

    updates to directory

--- Final_Project/directory-script.js
+++ Final_Project/directory-script.js
@@ -1,17 +1,79 @@
+const busImg1=document.getElementById("galImg1");
+const busImg2=document.getElementById("galImg2");
+const busImg3=document.getElementById("galImg3");
+const busImg4=document.getElementById("galImg4");
+const busImg5=document.getElementById("galImg5");
+const busImg6=document.getElementById("galImg6");
+const busImg7=document.getElementById("galImg7");
+const busNam1=document.getElementById("businessName1");
+const busNam2=document.getElementById("businessName2");
+const busNam3=document.getElementById("businessName3");
+const busNam4=document.getElementById("businessName4");
+const busNam5=document.getElementById("businessName5");
+const busNam6=document.getElementById("businessName6");
+const busNam7=document.getElementById("businessName7");
 
+
+fetch("https://moosius1.github.io/Final_Project/data/business-names.json")
+.then(function(data){
+    return data.json();
+})
+.then(function(jsonObject){
+    console.log(jsonObject);
+    imagesrc1=jsonObject["Businesses"][0]["logourl"];
+    imagesrc2=jsonObject["Businesses"][1]["logourl"];
+    imagesrc3=jsonObject["Businesses"][2]["logourl"];
+    imagesrc4=jsonObject["Businesses"][3]["logourl"];
+    imagesrc5=jsonObject["Businesses"][4]["logourl"];
+    imagesrc6=jsonObject["Businesses"][5]["logourl"];
+    imagesrc7=jsonObject["Businesses"][6]["logourl"];
+    busNam1.textContent=jsonObject["Businesses"][0]["name"];
+    busNam2.textContent=jsonObject["Businesses"][1]["name"];
+    busNam3.textContent=jsonObject["Businesses"][2]["name"];
+    busNam4.textContent=jsonObject["Businesses"][3]["name"];
+    busNam5.textContent=jsonObject["Businesses"][4]["name"];
+    busNam6.textContent=jsonObject["Businesses"][5]["name"];
+    busNam7.textContent=jsonObject["Businesses"][6]["name"];
+    document.getElementById("link1").href=jsonObject["Businesses"][0]["businessurl"];
+    document.getElementById("link2").href=jsonObject["Businesses"][1]["businessurl"];
+    document.getElementById("link3").href=jsonObject["Businesses"][2]["businessurl"];
+    document.getElementById("link4").href=jsonObject["Businesses"][3]["businessurl"];
+    document.getElementById("link5").href=jsonObject["Businesses"][4]["businessurl"];
+    document.getElementById("link6").href=jsonObject["Businesses"][5]["businessurl"];
+    document.getElementById("link7").href=jsonObject["Businesses"][6]["businessurl"];
+    document.getElementById("pnum1").textContent=jsonObject["Businesses"][0]["phonenumber"];
+    document.getElementById("pnum2").textContent=jsonObject["Businesses"][1]["phonenumber"];
+    document.getElementById("pnum3").textContent=jsonObject["Businesses"][2]["phonenumber"];
+    document.getElementById("pnum4").textContent=jsonObject["Businesses"][3]["phonenumber"];
+    document.getElementById("pnum5").textContent=jsonObject["Businesses"][4]["phonenumber"];
+    document.getElementById("pnum6").textContent=jsonObject["Businesses"][5]["phonenumber"];
+    document.getElementById("pnum7").textContent=jsonObject["Businesses"][6]["phonenumber"];
+    busImg1.src=imagesrc1;
+    busImg2.src=imagesrc2;
+    busImg3.src=imagesrc3;
+    busImg4.src=imagesrc4;
+    busImg5.src=imagesrc5;
+    busImg6.src=imagesrc6;
+    busImg7.src=imagesrc7;
+})
 
 function toggleMenu() {
     document.getElementsByClassName("links")[0].classList.toggle("responsive");
-    document.getElementsByClassName("menu")
+    
 
 
 }
+
+function toggleView(){
+    document.getElementsByClassName("gallery")[0].classList.toggle("listView");
+}
+
 
 date =  new Date();
 y = date.getFullYear();
 m = date.getMonth() + 1;
 d = date.getDate();
-document.getElementById("dates").innerHTML = m + "/" + d + "/" + y;
+
 
 
 
@@ -20,36 +82,5 @@ document.querySelector('#lastmod').textContent = document.lastModified;
 let thedate = new Date();
 
 
-if (thedate.getDay() ==0,1,2,3,4,5) {
-    document.querySelector('#banner').style.display = 'none'
-}
 
-document.addEventListener("DOMContentLoaded", function() {
-    var lazyloadImages = document.querySelectorAll("img.lazy");    
-    var lazyloadThrottleTimeout;
-    
-    function lazyload () {
-      if(lazyloadThrottleTimeout) {
-        clearTimeout(lazyloadThrottleTimeout);
-      }    
-      
-      lazyloadThrottleTimeout = setTimeout(function() {
-          var scrollTop = window.pageYOffset;
-          lazyloadImages.forEach(function(img) {
-              if(img.offsetTop < (window.innerHeight + scrollTop)) {
-                img.src = img.dataset.src;
-                
-              }
-          });
-          if(lazyloadImages.length == 0) { 
-            document.removeEventListener("scroll", lazyload);
-            window.removeEventListener("resize", lazyload);
-            window.removeEventListener("orientationChange", lazyload);
-          }
-      }, 20);
-    }
-    
-    document.addEventListener("scroll", lazyload);
-    window.addEventListener("resize", lazyload);
-    window.addEventListener("orientationChange", lazyload);
-  });
+
